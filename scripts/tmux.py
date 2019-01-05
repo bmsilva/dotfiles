3a38e6c2d36de727517aa98358da3e8a2c20c3df
Bruno Miguel Silva <bmsilva@gmail.com>
2019-01-05 17:49:13 +0000

    settings update

--- scripts/tmux.py
+++ scripts/tmux.py
@@ -2,27 +2,47 @@
 import os
 import subprocess
 
-print("TMUX: %s" % os.getenv("TMUX", ""))
-print("SSH_TTY: %s" % os.getenv("SSH_TTY", ""))
-print("SSH_AUTH_SOCK: %s" % os.getenv("SSH_AUTH_SOCK", ""))
-print("HOME: %s" % os.getenv("HOME", ""))
 
-if os.getenv("TMUX") is None:
-    if os.getenv("SSH_TTY") is not None:
-        if os.getenv("SSH_AUTH_SOCK") is not None:
-            sock_file = os.path.join(os.getenv("HOME"), ".wrap_auth_sock")
-            try:
-                #always try to remove
-                os.remove(sock_file)
-            except OSError:
-                pass
-            os.symlink(os.getenv("SSH_AUTH_SOCK"), sock_file)
-            os.environ['SSH_AUTH_SOCK'] = sock_file
+def is_valid(key):
+    if not key.startswith('PYENV'):
+        return True
 
-    try:
-        subprocess.check_call(["tmux", "attach-session", "-t", "sshwrap"])
-    except subprocess.CalledProcessError:
-        print("lets create session")
-        os.environ['STY'] = "tmux-sshwrap"
-        os.execlpe("tmux", "tmux", "new-session", "-s", "sshwrap",
-                   os.environ)
+    if key.endswith('_ROOT'):
+        return True
+
+    if key.endswith('_SHELL'):
+        return True
+
+    return False
+
+
+def main():
+    if os.getenv("TMUX") is None:
+        if os.getenv("SSH_TTY") is not None:
+            if os.getenv("SSH_AUTH_SOCK") is not None:
+                sock_file = os.path.join(os.getenv("HOME"), ".wrap_auth_sock")
+                try:
+                    #always try to remove
+                    os.remove(sock_file)
+                except OSError:
+                    pass
+                os.symlink(os.getenv("SSH_AUTH_SOCK"), sock_file)
+                os.environ['SSH_AUTH_SOCK'] = sock_file
+
+        try:
+            subprocess.check_call(["tmux", "attach-session", "-t", "sshwrap"])
+        except subprocess.CalledProcessError:
+            os.environ['STY'] = "tmux-sshwrap"
+            env = {k: os.environ[k] for k in os.environ.keys() if is_valid(k)}
+            os.execlpe("tmux", "tmux", "new-session", "-s", "sshwrap", env)
+    else:
+        print('TMUX var is set...')
+
+
+if __name__ == '__main__':
+    print("TMUX: %s" % os.getenv("TMUX", ""))
+    print("SSH_TTY: %s" % os.getenv("SSH_TTY", ""))
+    print("SSH_AUTH_SOCK: %s" % os.getenv("SSH_AUTH_SOCK", ""))
+    print("HOME: %s" % os.getenv("HOME", ""))
+
+    main()
